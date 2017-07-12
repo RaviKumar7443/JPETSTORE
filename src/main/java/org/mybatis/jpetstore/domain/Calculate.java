d05a7d6bc93830eb2990e1f3473363a8bbd55ae5
Ubuntu <utsav@jenkinsVM.fccfqjldplquxhhqqb35lusond.ix.internal.cloudapp.net>
2017-07-12 11:00:02 +0000

    Wed Jul 12 11:00:00 UTC 2017

--- src/main/java/org/mybatis/jpetstore/domain/Calculate.java
+++ src/main/java/org/mybatis/jpetstore/domain/Calculate.java
@@ -10,6 +10,8 @@ public void hello()
 System.out.println("JPET Store Application");
 System.out.println("Class name: Calculate.java");
 System.out.println("Hello World");
+System.out.println("Making a new Entry at Wed Jul 12 11:00:00 UTC 2017");
+System.out.println("Wed Jul 12 11:00:00 UTC 2017");
 System.out.println("Making a new Entry at Mon Jul 10 11:00:00 UTC 2017");
 System.out.println("Mon Jul 10 11:00:00 UTC 2017");
 System.out.println("Making a new Entry at Sat Jul  8 11:00:00 UTC 2017");
